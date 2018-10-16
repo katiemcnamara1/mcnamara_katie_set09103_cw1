e046074bcc4546af6bb913d67dc6d90d3c06afff
Katie McNamara <40280565@live.napier.ac.uk>
2018-10-16 14:56:52 +0100

    404 error page

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ def characters():
     name ='Fantine'
     return render_template('characters3.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
