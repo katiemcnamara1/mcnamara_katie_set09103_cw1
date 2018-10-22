83f3ac4331134e71961b65be057803bb4c821fd0
Katie McNamara <40280565@live.napier.ac.uk>
2018-10-22 10:53:07 +0100

    404

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def film4():
 @app.errorhandler(404)
 def four(e):
     name = '404'
-    return render_template('characters2.html' , name=name), 404
+    return render_template('characters2.html', name=name), 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
